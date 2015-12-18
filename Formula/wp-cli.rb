978867441a0fbe8d33f546dc76a53cbce39b572b
Joshua Priddle <jpriddle@me.com>
2015-12-18 16:45:04 -0500

    Add HEAD version of WP-CLI

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,12 +1,13 @@
-require 'formula'
+require "formula"
 
 class WpCli < Formula
   homepage "http://wp-cli.org/"
   version  "0.21.1"
   url      "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
   sha256   "acd8a1f90610c9174158366f1c9016fe1d0f596311ca53e4d0c4d1c497c15f6a"
+  head     "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar"
 
   def install
-    bin.install "wp-cli-#{self.class.version}.phar" => "wp"
+    bin.install "wp-cli-#{build.head? ? "nightly" : version}.phar" => "wp"
   end
 end
