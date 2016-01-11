baf57f8ce8605512d8cf9bafe38f17ac73afc60c
Joshua Priddle <jpriddle@me.com>
2016-01-11 12:50:09 -0500

    Update wp-cli

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class WpCli < Formula
   homepage "http://wp-cli.org/"
-  version  "0.21.1"
+  version  "0.22.0"
   url      "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
-  sha256   "acd8a1f90610c9174158366f1c9016fe1d0f596311ca53e4d0c4d1c497c15f6a"
+  sha256   "9ad3d193c19771b13696ed8e6a5e59d2fd3acc12464ec119aae9238b8aca7274"
   head     "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar"
 
   def install
