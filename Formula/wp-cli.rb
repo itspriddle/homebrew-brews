2772ab6232dda24bf19146c556040e7ba4fa7083
Joshua Priddle <jpriddle@me.com>
2016-11-08 01:43:18 -0500

    Bump WP-CLI version

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,8 +1,8 @@
 class WpCli < Formula
   homepage "http://wp-cli.org/"
-  version  "0.24.1"
+  version  "0.25.0"
   url      "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
-  sha256   "cdc15ba1416c090262a0694edd4e2b656dcbd9ef5119258930d92d8c2e65f608"
+  sha256   "20afc72b502d072bc26e95c1e61c12554277a56eb7451df2b92a46d32393bb97"
   head     "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar"
 
   def install
