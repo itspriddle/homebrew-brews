7d034b9644bcb374e10dd57e9d3e7d0321534b62
Joshua Priddle <jpriddle@me.com>
2016-11-10 19:31:40 -0500

    Bump composer version

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -1,8 +1,8 @@
 class Composer < Formula
   homepage "https://getcomposer.org/download"
-  version  "1.2.1"
+  version  "1.2.2"
   url      "https://getcomposer.org/download/#{version}/composer.phar"
-  sha256   "c2e04040b807a8530e5c83de56bdaaf63a0f183f8fd449bbe6e41f660e647427"
+  sha256   "c6ae4e099351233f5f82ade9738475eec6587aadf9ffe66fec4c350b329fbb32"
   head     "https://getcomposer.org/composer.phar"
 
   def install
