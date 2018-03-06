35c716c112b0c8421871261a6bcb0ef93075120b
Joshua Priddle <jpriddle@me.com>
2018-03-06 16:53:38 -0500

    Upgrade composer to 1.6.3

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -1,8 +1,8 @@
 class Composer < Formula
   homepage "https://getcomposer.org/download"
-  version  "1.2.2"
+  version  "1.6.3"
   url      "https://getcomposer.org/download/#{version}/composer.phar"
-  sha256   "c6ae4e099351233f5f82ade9738475eec6587aadf9ffe66fec4c350b329fbb32"
+  sha256   "52cb7bbbaee720471e3b34c8ae6db53a38f0b759c06078a80080db739e4dcab6"
   head     "https://getcomposer.org/composer.phar"
 
   def install
