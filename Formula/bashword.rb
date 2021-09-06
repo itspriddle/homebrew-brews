c5e7d01b241a24debc544626a0dff68230609934
Joshua Priddle <jpriddle@me.com>
2021-09-06 13:37:20 -0400

    Update bashword.rb

--- Formula/bashword.rb
+++ Formula/bashword.rb
@@ -1,9 +1,9 @@
 class Bashword < Formula
-  version "0.1.0"
+  version "0.2.0"
 
   homepage "https://github.com/itspriddle/bashword"
   url      "https://github.com/itspriddle/bashword/archive/v#{version}.tar.gz"
-  sha256   "0161a903bf1d4e9dd9dccaa53f525d0f2287546a02ad6e0d9d6689f5bea0331d"
+  sha256   "111fdcfe1fe0450003ce84bc0014d5b849c8e6524abc59c98941cedf6c202452"
 
   head "https://github.com/itspriddle/bashword.git"
 
