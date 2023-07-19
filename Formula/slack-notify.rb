ff3d9858f251b5f1600b6885a429354b82065fb4
Joshua Priddle <jpriddle@me.com>
2023-07-19 17:53:34 -0400

    Update slack-notify.rb to v0.0.2

--- Formula/slack-notify.rb
+++ Formula/slack-notify.rb
@@ -1,9 +1,9 @@
 class SlackNotify < Formula
-  version "0.0.1"
+  version "0.0.2"
 
   homepage "https://github.com/itspriddle/slack-notify"
   url      "https://github.com/itspriddle/slack-notify/archive/v#{version}.tar.gz"
-  sha256   "cdb74834a16016e65d7fcf649e6bfef09c15c01df6ce11bd3ed9c2aad5f243cc"
+  sha256   "2cfbed59688dbc74a1341b09f885216bf3bddd8302853cb3f4911f73373eafd4"
 
   head "https://github.com/itspriddle/slack-notify.git"
 
