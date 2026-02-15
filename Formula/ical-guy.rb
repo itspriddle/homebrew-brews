aea18e9ba65c90e4989278b0ddf306916c909f64
Josh Priddle <jpriddle@me.com>
2026-02-15 19:58:19 +0000

    Updated ical-guy to v0.3.0

--- Formula/ical-guy.rb
+++ Formula/ical-guy.rb
@@ -1,10 +1,10 @@
 class IcalGuy < Formula
-  version "0.2.0"
+  version "0.3.0"
 
   desc "CLI for querying macOS calendar events, outputting JSON"
   homepage "https://github.com/itspriddle/ical-guy"
   url "https://github.com/itspriddle/ical-guy/releases/download/v#{version}/ical-guy-v#{version}-macos-universal.tar.gz"
-  sha256 "1406534e4f5960af2954cfeddc7cca7c831cca6f10f67700cca72945d2665cbf"
+  sha256 "1f3363019a693b40eeeb4587034754e39c4b3c8e7e3362a94ef5586b1252b5b4"
   license "MIT"
 
   depends_on :macos
