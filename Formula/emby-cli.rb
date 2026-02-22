bb382cabbb35d3c1eef4b3299517b3bab56ee6b8
Josh Priddle <jpriddle@me.com>
2026-02-22 20:44:01 +0000

    Updated emby-cli to v0.2.3

--- Formula/emby-cli.rb
+++ Formula/emby-cli.rb
@@ -1,5 +1,5 @@
 class EmbyCli < Formula
-  version "0.2.2"
+  version "0.2.3"
 
   desc "Command-line interface for Emby media servers"
   homepage "https://github.com/itspriddle/emby-cli"
@@ -9,12 +9,12 @@ class EmbyCli < Formula
 
   on_arm do
     url "https://github.com/itspriddle/emby-cli/releases/download/v#{version}/emby-aarch64-apple-darwin.tar.gz"
-    sha256 "a022fd0b9622e410db02734b0d29bd2b036a6cf0c355112758a01d55e6b9da11"
+    sha256 "f8a4637a789a1a351b9592cda8535eb086ed06f30c4521dee4e13888581a27e5"
   end
 
   on_intel do
     url "https://github.com/itspriddle/emby-cli/releases/download/v#{version}/emby-x86_64-apple-darwin.tar.gz"
-    sha256 "35af7733713ffacae89aab4952c57ab67a0f7ec3cb86788004b31f2d996b3d53"
+    sha256 "17bfe2e8267f4ea07e64649a5ae33f711d21fcf050ae9513fc3e112d98c9a5a3"
   end
 
   def install
